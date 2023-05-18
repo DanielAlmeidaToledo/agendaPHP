e1d7fb2c008eaafaa7703b802d25e7fbb3ab8685
Daniel Toledo <dan992965@gmail.com>
2023-05-18 00:11:34 -0300

    feature: adiciona estilizacao global nas telas

--- Paginas/formularioInclusao.php
+++ Paginas/formularioInclusao.php
@@ -8,8 +8,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="../styles/global.css">
-    <link rel="stylesheet" href="../styles/formularioInclusao.css">
-    <title>Agenda - Daniel Toledo</title>
+    <title>Agenda - Inclusão</title>
 </head>
 <body>
 
